b5e6126db4e7a46569e6f5ee743e2219359ea65d
Jon Cheng <jon.syndicate@gmail.com>
2015-03-25 12:34:12 -0400

    added starter code for todo list

---
+++ Week_08_forms_and_things/16_to_do_list/starter_code/js/main.js
@@ -0,0 +1,21 @@
+// Initial Values
+
+// Attachments
+// Here, we attach certain function to certain events
+// Below, we elaborate on what each function does
+
+// When the #new form is submitted, add the new item
+
+// When an item gets clicked, mark as complete or incomplete
+
+// When a remove link is clicked, remove that item
+
+// When an edit link is clicked, go into edit mode
+
+// When an item editor is submitted, save the changes
+
+// When a user leaves an item editor form, save the changes
+
+// When the Clear List button is clicked, clear out the items 
+
+// When the Clear Completed button is clicked, clear out the completed items
